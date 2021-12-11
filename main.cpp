fb92700625df831d76968d7ee0a88c1d9cd4f9b2
dmitrytrc <9984398@gmail.com>
2021-12-11 22:07:37 +0300

    CMake : Output switched to /bin directory

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, Marks!" << std::endl;
+
+/** Entry point for Marks application
+ *
+ * @param argc Argument count
+ * @param argv Argument values
+ * @return 0 if successful
+ */
+int main (int argc, char *argv[]) {
+    if (argc < 2) {
+        std::cout << "Interactive mode selected." << std::endl;
+    } else {
+        std::cout << "Console mode selected." << std::endl;
+    }
+
     return 0;
 }
