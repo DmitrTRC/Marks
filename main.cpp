183721224eb43815b7d64af6d0a7989ed90bf08d
dmitrytrc <9984398@gmail.com>
2021-12-16 20:49:26 +0300

    main() : Interactive mode  :  Refactored.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
 
+void log_menu () {
+
+    std::cout << "Menu:" << std::endl;
+};
+
 // 2 Modes : Console and Interactive
 int main (int argc, char *argv[]) {
     if (argc >= 2) {
@@ -12,6 +17,7 @@ int main (int argc, char *argv[]) {
 
     } else {
         std::cout << "Interactive mode" << std::endl;
+        log_menu ();
     }
     return 0;
 }
