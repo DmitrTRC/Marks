824c675fa43ae8129cb6c0b6c0f84ad276fb255e
dmitrytrc <9984398@gmail.com>
2021-12-16 20:40:42 +0300

    main() : Console / Interactive  mode added.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, Marks!" << std::endl;
+
+// 2 Modes : Console and Interactive
+int main (int argc, char *argv[]) {
+    if (argc >= 2) {
+        std::cout << "Console mode" << std::endl;
+        for (int i = 1; i < argc; i++) {
+            std::cout << argv[i] << std::endl;
+        }
+
+
+    } else {
+        std::cout << "Interactive mode" << std::endl;
+    }
     return 0;
 }
