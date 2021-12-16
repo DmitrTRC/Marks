3165904fceb71a04306c702b67f416a0197fa096
dmitrytrc <9984398@gmail.com>
2021-12-16 23:37:17 +0300

    main() : Home task added.

--- main.cpp
+++ main.cpp
@@ -14,8 +14,6 @@ int main (int argc, char *argv[]) {
         for (int i = 1; i < argc; i++) {
             std::cout << argv[i] << std::endl;
         }
-
-
     } else {
         std::cout << "Interactive mode" << std::endl;
         log_menu ();
