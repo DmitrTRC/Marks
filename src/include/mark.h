0187a0ca7a054820e31ce066cad2f1a6006f705a
dmitrytrc <9984398@gmail.com>
2021-12-22 19:07:15 +0300

    markTypes and markValues maps added.

--- src/include/mark.h
+++ src/include/mark.h
@@ -7,6 +7,7 @@
 
 #include <iostream>
 #include <vector>
+#include <map>
 
 
 enum class MarkType {
@@ -18,6 +19,15 @@ enum class MarkType {
     Final
 };
 
+std::map<std::string, MarkType> markTypeMap = {
+        {"None",     MarkType::None},
+        {"HomeWork", MarkType::HomeWork},
+        {"Lesson",   MarkType::Lesson},
+        {"Test",     MarkType::Test},
+        {"Exam",     MarkType::Exam},
+        {"Final",    MarkType::Final}
+};
+
 enum class MarkValue {
     None,
     VeryBad,
@@ -27,12 +37,26 @@ enum class MarkValue {
     Excellent
 };
 
+std::map<std::string, MarkValue> markValueMap = {
+        {"None",      MarkValue::None},
+        {"VeryBad",   MarkValue::VeryBad},
+        {"Bad",       MarkValue::Bad},
+        {"Normal",    MarkValue::Normal},
+        {"VeryGood",  MarkValue::VeryGood},
+        {"Excellent", MarkValue::Excellent},
+        {"0",         MarkValue::None},
+        {"1",         MarkValue::VeryBad},
+        {"2",         MarkValue::Bad},
+        {"3",         MarkValue::Normal},
+        {"4",         MarkValue::VeryGood},
+        {"5",         MarkValue::Excellent}
+};
 
 
 class Mark {
 public:
-    Mark (const std::string &subject, const MarkValue& value, ) : subject (subject), value (value),
-                                                                             weight (weight) {}
+    Mark (const std::string &subject, const MarkValue &value,) : subject (subject), value (value),
+                                                                 weight (weight) {}
 
     std::string subject;
     MarkValue value;
