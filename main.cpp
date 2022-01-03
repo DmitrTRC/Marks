60f7ad410a1cc45abb3d19307ad8af5be8031e71
dmitrytrc <9984398@gmail.com>
2022-01-03 22:45:44 +0300

    HomeTask 16  : Added.

--- main.cpp
+++ main.cpp
@@ -4,28 +4,31 @@
 #include <string>
 
 
+// int log_menu();
 std::string log_menu () {
-//* Menu example:
-//*  Logging menu
-//* 1. Add Entry
-//* 2. Delete Entry
-//* 3. Print Entries
-//* 4. Exit
-//* Enter your choice:
 
-std::cout << "Menu:" << std::endl;
-std::cout << "Marks" << std::endl;
-std::cout << "Add Marks" << std::endl;
-std::cout << "Delete Marks" << std::endl;
-std::cout << "Exit" << std::endl;
-std::cout << "Enter your choice: " << std::endl;
-std::string choice;
-std::cin >>choice;
-return choice;
+//TODO: Implement menu numeration
+    std::cout << "Menu:" << std::endl;
+    std::cout << "Show Marks log " << std::endl;
+    std::cout << "Add Marks" << std::endl;
+    std::cout << "Delete Marks" << std::endl;
+    std::cout << "Exit" << std::endl;
+    std::cout << "Enter your choice: " << std::endl;
+    std::string choice;
+    std::cin >> choice;
+    return choice;
 // Checked : Ok : 5
 };
 
-   
+//TODO: Implement menu dispatcher
+//  void dispatch_menu_choice (int choice) {
+//  int choice = log_menu();
+//if ( choice  == 1) {
+//   std::cout << "Marks : List" << std::endl;
+//}
+//  }
+
+
 
 
 // 2 Modes : Console and Interactive
