b25b03a832c6a1b6db495692e36455f357f0d939
Dmitry Morozov <9984398@gmail.com>
2022-01-28 00:33:57 +0300

    NC

---
+++ number.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <cassert>
+
+
+int get_uniq_sentence(std::vector<int> &numbers)
+/*
+Find number with occurence not divisible by 3 in sequence
+*/
+{
+    
+
+}
+
+int main()
+{
+    std::vector<int> nums = {1, 1, 1, 2, 2, 2, 3, 3, 4, 4, 4};
+    std::cout << get_number(nums) << std::endl;
+}
