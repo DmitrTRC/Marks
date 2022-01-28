c84aa1b1b5a2f48f8fa63fcdb6e0dad84d4d7d83
Dmitry Morozov <9984398@gmail.com>
2022-01-28 16:49:53 +0300

    70%  Done

--- number.cpp
+++ number.cpp
@@ -1,19 +1,38 @@
 #include <iostream>
 #include <vector>
 #include <cassert>
+#include <numeric>
 
+int decimal_to_ternary (int n) {
+    assert (n >= 0);
+    int result = 0;
+    int base = 1;
+    while (n > 0) {
+        result += n % 3 * base;
+        n /= 3;
+        base *= 10;
+    }
+    return result;
+    }
 
-int get_uniq_sentence(std::vector<int> &numbers)
-/*
-Find number with occurence not divisible by 3 in sequence
-*/
-{
-    
+void convert_vector_to_ternary (std::vector<int> &v) {
+    for (auto &i : v) {
+        i = decimal_to_ternary (i);
+    }
+}   
 
+int find_number(std::vector<int> &numbers) {
+    convert_vector_to_ternary(numbers);
+    unsigned int result =  std::accumulate(numbers.begin(), numbers.end(), 0, [](int lhs, int rhs) { 
+        return lhs ^ rhs;
+    });
+    return result;
 }
 
 int main()
 {
     std::vector<int> nums = {1, 1, 1, 2, 2, 2, 3, 3, 4, 4, 4};
-    std::cout << get_number(nums) << std::endl;
+    for ( int i = 0; i < nums.size(); i++ ) {
+        std::cout << decimal_to_ternary(nums[i]) << std::endl;
+    }
 }
